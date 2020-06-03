2a5dae5bfec5f604f8746702918b5ebaafe5460f
o.diachenko.2005 <o.diachenko.2005@in.np.gov.ua>
2020-06-03 22:02:32 +0300

    add sign-protocol component

--- src/boot/register-db-eds.js
+++ src/boot/register-db-eds.js
@@ -3,6 +3,7 @@ import edsStore from '../store';
 import {euSignMedia, initEuSignMedia } from '../lib/eu-sign-media';
 import {euSignFile, initEuSignFile } from '../lib/eu-sign-file';
 import SignForm from '../component/SignForm.vue';
+import SignProtocol from '../component/SignProtocol.vue';
 
 // register store module
 store.registerModule('eds', edsStore);
@@ -17,7 +18,8 @@ export default ({ Vue }) => {
   Vue.prototype.$euSignFile = euSignFile;
   Vue.prototype.$euSignMedia = euSignMedia;
 
-  // globally register component
+  // globally register components
   Vue.component('sign-form', SignForm);
+  Vue.component('sign-protocol', SignProtocol);
 
 }
